4eb2b760fe1eee49e50acb26779e4896915e67e5
Antti Kautiainen <antti@kautiainen.com>
2024-11-08 14:21:29 +0200

    init: form input module Changes to be committed: -	new file:   index.js   - feat: entry point for the module -	new file:   package.json   - config: The project configuration for webpack 5 -	new file:   src'   - feat: the source folder -	new file:   webpack.config.js   - config: WebPack configuration for building a the development   bundle.

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/**
+ * The form input module main file.
+ * @module forminput
+ */
+
+//
+// This file combines the form input components into a single bundle component
+//
+
+// The default exports.
+export default {
+
+}
