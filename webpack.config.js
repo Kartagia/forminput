e88e4d344d2e146daffbb67d28ab1d070d3cca3d
Antti Kautiainen <antti@kautiainen.com>
2024-11-08 14:24:19 +0200

    Changes to be committed: -	modified:   webpack.config.js   - config-fix: changing the development bundle target to contain   indication it is development bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'forminput.js',
+    filename: 'forminput.dev.js',
   },
   module: {
     rules: [
